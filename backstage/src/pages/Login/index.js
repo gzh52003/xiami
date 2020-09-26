687378874f7c886569b76a1592a222237f158faa
zeshan <7849282+lzszeshan@user.noreply.gitee.com>
2020-09-26 10:44:39 +0800

    backlogin

--- backstage/src/pages/Login/index.js
+++ backstage/src/pages/Login/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Form, Input, Button, Checkbox } from 'antd';
+import React,{useState,useEffect,useMemo,useContext} from 'react';
+import { Form, Input, Button, Checkbox,Alert } from 'antd';
+import {request} from '../../utils/index'
 import { HomeOutlined, FileAddOutlined, ReadOutlined, AudioOutlined } from '@ant-design/icons';
 import './Login.css';
 
@@ -22,10 +23,24 @@ const tailLayout = {
 
 
 function Login(props) {
-     const gotohome = ()=>{}
-    const onFinish = (values) => {
-        console.log('Success:', values);
-    };
+     
+    const onFinish = async ({username,password,mdl}) => {
+        // password = CryptoJS.SHA256(password);
+        // password = CryptoJS.enc.Hex.stringify(password)
+        // console.log(password)
+        const data = await request.get('/backlogin',{
+            username,
+            password,
+            mdl
+        });
+        console.log('user=',data);
+        if(data.code === 1){
+            // 跳转到我的页面
+            props.history.push('./Home')
+            // 把用户信息存入本地（数据持久化）
+            localStorage.setItem('currentUser',JSON.stringify(data.data));
+        }
+      };
 
     const onFinishFailed = (errorInfo) => {
         console.log('Failed:', errorInfo);
@@ -43,7 +58,7 @@ function Login(props) {
         >
             <Form.Item
                 label="用户名"
-                name="用户名"
+                name="username"
                 rules={[
                     {
                         required: true,
@@ -56,7 +71,7 @@ function Login(props) {
 
             <Form.Item
                 label="密码"
-                name="密码"
+                name="password"
                 rules={[
                     {
                         required: true,
@@ -67,9 +82,9 @@ function Login(props) {
                 <Input.Password />
             </Form.Item>
            
-            <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+            <Form.Item {...tailLayout} name="mdl" valuePropName="checked">
                 <Checkbox>七天免登陆</Checkbox>
-                       <Button type="danger" htmlType="submit" onClick={props.gotohome} style={{marginTop:"5px"}}>
+                       <Button type="danger" htmlType="submit"  style={{marginTop:"5px"}}>
                     登录
         </Button>
             </Form.Item>
--- backstage/src/utils/request.js
+++ backstage/src/utils/request.js
@@ -5,7 +5,7 @@
 import axios from 'axios';
 
 const request = axios.create({
-    baseURL:'http://localhost:2003/',
+    baseURL:'http://localhost:2003/api',
     withCredentials:true
 })
 
